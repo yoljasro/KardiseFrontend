f09e61407b7068452408dc2ed91c8d6f7d2ed076
yoljasro <saidaliyevjasur450@gmail.com>
2024-01-31 21:17:06 +0500

    changes

---
+++ src/components/Btn/index.tsx
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+//styles
+import styles from "./index.module.sass";
+// FC
+import { FC } from "react";
+// next components
+import Image from "next/image";
+import Link from "next/link";
+// react icons
+import { FaTwitter, FaFacebookF, FaLinkedinIn, FaYoutube } from "react-icons/fa";
+
+export const Btn: FC<any> = () => {
+    return (
+        <div className={styles.btn}>
+            
+        </div>
+    );
+};
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -8,11 +8,16 @@ import Image from "next/image";
 import Link from "next/link";
 // react icons
 import { FaTwitter, FaFacebookF, FaLinkedinIn, FaYoutube } from "react-icons/fa";
+import { Btn } from "../Btn";
 
 export const Header: FC<any> = () => {
     return (
         <div className={styles.header}>
-
+            <div  className={styles.header__info}>
+                <p className={styles.header__text}>Лидер - в сфере проектирования
+                    и строительства</p>
+                    <Btn/>
+            </div>
         </div>
     );
 };

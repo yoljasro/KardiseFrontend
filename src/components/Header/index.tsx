8fc725d746ffa7357a3c92952bc3ca062d7d70a8
yoljasro <saidaliyevjasur450@gmail.com>
2024-02-02 20:08:16 +0500

    Header fixed

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -13,10 +13,40 @@ import { Btn } from "../Btn";
 export const Header: FC<any> = () => {
     return (
         <div className={styles.header}>
-            <div  className={styles.header__info}>
+            <div className={styles.header__info}>
                 <p className={styles.header__text}>Лидер - в сфере проектирования
                     и строительства</p>
-                    <Btn/>
+                <Btn />
+            </div>
+            <div className={styles.header__cards}>
+                <div className={styles.header__card}>
+                    <p className={styles.header__title}>13+</p>
+                    <p className={styles.header__subTitle}>Завершенные Проекты</p>
+                </div>
+
+                <div className={styles.header__cardTwo}>
+                    <p className={styles.header__title}>26+</p>
+                    <p className={styles.header__subTitle}>Лет Опыта</p>
+                </div>
+
+                <div className={styles.header__directions}>
+                    <p className={styles.header__companyText}>Основными направлениями
+                        деятельности компании KARDISE являются:</p>
+                    <div className={styles.header__content}>
+                        <div className={styles.header__media}>
+                            <Image src={'/assets/img/shape.svg'} alt="shape" width={61} height={63} />
+                            <p className={styles.header__subInfo}>Строительство и девелопмент</p>
+                        </div>
+                        <div className={styles.header__media}>
+                            <Image src={'/assets/img/ruler.svg'} alt="shape" width={61} height={63} />
+                            <p className={styles.header__subInfo}>Инвестиции в землю</p>
+                        </div>
+                        <div className={styles.header__media}>
+                            <Image src={'/assets/img/measuring-tool.svg'} alt="shape" width={61} height={63} />
+                            <p className={styles.header__subInfo}>Инвестиции в сфере обслуживание <br />   машин и механизмов</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );

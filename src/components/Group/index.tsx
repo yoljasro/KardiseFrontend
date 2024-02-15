f804f782e4a8a0eaa9e0ef87f174b0952b4b5956
yoljasro <saidaliyevjasur450@gmail.com>
2024-02-15 12:21:46 +0500

    group component in progress

--- src/components/Group/index.tsx
+++ src/components/Group/index.tsx
@@ -11,7 +11,21 @@ import Link from "next/link";
 export const Group: FC<any> = () => {
     return (
         <div className={styles.group}>
-            df
+            <div className={styles.group__person}>
+                <div className={styles.group__card}>
+                    <p className={styles.group__cardTitle}>26+</p>
+                    <p className={styles.group__cardSubTitle}>ГОДЫ ОПЫТА </p>
+                </div>
+            </div>
+
+            <div className={styles.group__info}>
+                <p className={styles.group__title}>О НАС </p>
+                <p className={styles.group__desc}>Мы предоставляем лучшее <br /> обслуживание клиентов</p>
+                <div className={styles.group__storyCard}>
+                    <p className={styles.group__storyCardInfo}>Историянашегоразвития—это иллюстрация прочных и надежных стремлений нашей созидательной работы, многогранной трудовой деятельности коллектива компании,того,как мы постепенно продвигаясь,смогли предвидетьи просчитать будущие шаг и на многое годы вперед.</p>
+                    <p className={styles.group__storyCardName}> Kardise group</p>
+                </div>
+            </div>
         </div>
     );
 };

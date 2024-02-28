68a5ed3027bd58ff0414cc21aa8c6c3358d3208d
yoljasro <saidaliyevjasur450@gmail.com>
2024-02-28 09:00:15 -0800

    bug fixing

--- src/components/Clients/index.tsx
+++ src/components/Clients/index.tsx
@@ -5,42 +5,20 @@ import styles from "./index.module.sass";
 import { Client } from '../../types';
 import { clientsAPI } from '../API';
 import { FC } from "react";
-import Image from "next/image";
 import { Fade } from "react-reveal";
 
 export const Clients: FC<any> = () => {
     const [clients, setClients] = useState<any>([]);
     const videoRef = useRef<HTMLVideoElement>(null);
-    const imageRef = useRef<HTMLImageElement>(null);
 
     const handleVideoClick = () => {
         if (videoRef.current) {
             const video = videoRef.current;
 
             if (video.paused) {
-                video.play();
-                video.requestFullscreen();
+                video.play().then(() => video.requestFullscreen());
             } else {
                 video.pause();
-            }
-        }
-    };
-
-    const handleImageClick = (client: Client) => {
-        if (videoRef.current && imageRef.current) {
-            const video = videoRef.current;
-            const videoSource = `http://152.42.162.108${client.video}`;
-            const image = imageRef.current;
-
-            if (video.paused) {
-                video.src = videoSource;
-                video.load();
-                video.play();
-                video.requestFullscreen();
-                image.style.display = "none"; // Rasmni yashirish
-            } else {
-                video.pause();
-                image.style.display="none"
             }
         }
     };
@@ -68,21 +46,19 @@ export const Clients: FC<any> = () => {
                     <div className={styles.client__cards}>
                         {clients.map((client: Client) => (
                             <div className={styles.client__videos} key={client._id}>
-                                <div className={styles.client__video} onClick={() => handleImageClick(client)}>
-                                    <Image ref={imageRef} className={styles.client__image} alt="image" src="/assets/img/uzumPage.png" width={300} height={200} />
+                                <div className={styles.client__video} onClick={handleVideoClick}>
                                     <div className={styles.client__play}>
                                         <div className={styles.client__triangle}></div>
                                     </div>
-                                    <video ref={videoRef} className={styles.client__video__video}>
-                                        <source src="" type="video/mp4" />
+                                    <video ref={videoRef} className={styles.client__video__video} width="100%" height="200">
+                                        <source src={`http://152.42.162.108${client.video}`} type="video/mp4" />
                                     </video>
                                 </div>
                             </div>
-                        ))}     
+                        ))}
                     </div>
                 </div>
             </Fade>
         </>
     );
 };
-
